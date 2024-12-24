525f34e0ae4ea70b28f0f8cf55f183eaef80fbac
machete <onlymamlya@gmail.com>
2024-12-25 01:29:02 +0200

    Initial commit

--- ConsoleApp4/obj/Debug/net8.0/ConsoleApp4.AssemblyInfo.cs
+++ ConsoleApp4/obj/Debug/net8.0/ConsoleApp4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26d2eb2848c596ca8931e414a35d6ca40a3be8ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
